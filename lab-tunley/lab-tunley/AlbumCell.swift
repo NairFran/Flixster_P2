7b166414f58e2bc4e2d87aa2a6c5af65dab025fe
Ariadna <98125876+NairFran@users.noreply.github.com>
2023-02-12 21:34:26 -0500

    Main
    Added the whole project.

---
+++ lab-tunley/lab-tunley/AlbumCell.swift
@@ -0,0 +1,14 @@
+//
+//  AlbumCell.swift
+//  lab-tunley
+//
+//  Created by Walter Franchino on 2/12/23.
+//
+
+import UIKit
+
+class AlbumCell: UICollectionViewCell {
+    
+    @IBOutlet weak var albumImageView: UIImageView!
+    
+}
